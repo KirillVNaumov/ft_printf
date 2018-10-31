37a2c3da74ea34c5e127dac4551803f192994407
Kirill Naumov <knaumov@e1z2r3p44.42.us.org>
2018-10-31 16:13:13 -0700

    norme

--- srcs/conversion_c.c
+++ srcs/conversion_c.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   conversion_c.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: knaumov <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/31 16:07:01 by knaumov           #+#    #+#             */
+/*   Updated: 2018/10/31 16:07:23 by knaumov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-char    *conversion_c(va_list *arg)
+char		*conversion_c(va_list *arg)
 {
-    char    *str;
-    int    tmp;
+	char	*str;
+	int		tmp;
 
-    tmp = va_arg(*arg, int);
-    str = ft_bchar(1, (char)tmp);
-    return (str);
+	tmp = va_arg(*arg, int);
+	str = ft_bchar(1, (char)tmp);
+	return (str);
 }
